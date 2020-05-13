02385c45e6f7f5c72623a69bafc1593926b89f98
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-13 07:29:17 -0400

    12. Decoupled component communication using a shared Service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,7 @@ import { HttpClient } from "@angular/common/http";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
 import { CoursesService } from "../services/courses.service";
+import { LoadingService } from "../loading/loading.service";
 
 @Component({
   selector: "home",
@@ -27,7 +28,10 @@ export class HomeComponent implements OnInit {
 
   advancedCourses$: Observable<Course[]>;
 
-  constructor(private coursesService: CoursesService) {}
+  constructor(
+    private coursesService: CoursesService,
+    private loadingService: LoadingService
+  ) {}
 
   ngOnInit() {
     // imperative style of calling the service
