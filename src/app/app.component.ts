1812b1bf7de018b58c8d4689be70b1feda06e88f
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-14 07:22:00 -0400

    28. Adapting the UI according to the user Authentication status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { LoadingService } from "./loading/loading.service";
 import { MessagesService } from "./messages/messages.service";
+import { AuthStore } from "./services/auth.service";
 
 @Component({
   selector: "app-root",
@@ -9,9 +10,11 @@ import { MessagesService } from "./messages/messages.service";
   // providers: [LoadingService, MessagesService],
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(public authStore: AuthStore) {}
 
   ngOnInit() {}
 
-  logout() {}
+  logout() {
+    this.authStore.logout();
+  }
 }
