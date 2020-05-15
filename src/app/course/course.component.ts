9c5feedf1484165a685c3b3646e974a5c52ddb6d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-05-14 20:11:34 -0400

    36. Reactive Angular - The Single Data Observable Pattern

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -21,10 +21,23 @@ import {
   shareReplay,
   catchError,
 } from "rxjs/operators";
-import { merge, fromEvent, Observable, concat, throwError } from "rxjs";
+import {
+  merge,
+  fromEvent,
+  Observable,
+  concat,
+  throwError,
+  combineLatest,
+} from "rxjs";
 import { Lesson } from "../model/lesson";
 import { CoursesStore } from "../services/courses-store.service";
 import { CoursesService } from "../services/courses.service";
+import { LoadingService } from "../loading/loading.service";
+
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
 
 @Component({
   selector: "course",
@@ -33,17 +46,32 @@ import { CoursesService } from "../services/courses.service";
 })
 export class CourseComponent implements OnInit {
   course$: Observable<Course>;
-
   lessons$: Observable<Lesson[]>;
+  data$: Observable<CourseData>;
 
   constructor(
     private route: ActivatedRoute,
-    private coursesService: CoursesService
+    private coursesService: CoursesService,
+    private loadingService: LoadingService
   ) {}
 
   ngOnInit() {
     const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
     this.course$ = this.coursesService.loadCourseById(courseId);
     this.lessons$ = this.coursesService.loadCourseLessons(courseId);
+    // using combineLatest to combine the data of these two observables into one observable
+    // Here there is no dependency between the observables i.e. one observable do not wait for the other observable to complete.
+    // when any of the observable completes, the combinelatest method returns the data
+    this.loadingService.loadingOn();
+    this.data$ = combineLatest([this.course$, this.lessons$]).pipe(
+      tap(() => this.loadingService.loadingOff()),
+      map(([course, lessons]) => {
+        console.log(course, lessons);
+        return {
+          course,
+          lessons,
+        };
+      })
+    );
   }
 }
